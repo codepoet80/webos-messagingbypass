9030090d8d217596895f61395250c6748a74c2e3
codepoet80 <jonathan.m.wise@gmail.com>
2022-09-18 14:50:07 -0400

    tweak layout

--- app/app/assistants/first-assistant.js
+++ app/app/assistants/first-assistant.js
@@ -2,15 +2,34 @@ function FirstAssistant() {
 }
 
 FirstAssistant.prototype.setup = function() {
-	this.controller.get("app-title").update(Mojo.appInfo.title);
-	this.controller.get("app-id").update(Mojo.appInfo.id);
-	this.controller.get("app-version").update(Mojo.appInfo.version);
+	this.controller.setupWidget("btnLaunchApp", {}, {
+		label : "Launch Accounts App",
+		disabled: false
+	});
 };
 
 FirstAssistant.prototype.activate = function(event) {
+	Mojo.Event.listen(this.controller.get("btnLaunchApp"),Mojo.Event.tap, this.handleButton); 
+};
+
+FirstAssistant.prototype.handleButton = function() {
+	params = {};
+	params.id = "com.palm.app.accounts";
+    this.launchRequest = new Mojo.Service.Request("palm://com.palm.applicationManager", {
+        method: "open",
+        parameters: params,
+        onSuccess: function(response) {
+            Mojo.Log.info("App Launch Success", JSON.stringify(response));
+        },
+        onFailure: function(response) {
+            Mojo.Log.error("App Launch Failure", JSON.stringify(response));
+        }
+    });
+    return true;
 };
 
 FirstAssistant.prototype.deactivate = function(event) {
+	Mojo.Event.stopListening(this.controller.get("btnLaunchApp"), Mojo.Event.tap, this.handleButton);
 };
 
 FirstAssistant.prototype.cleanup = function(event) {
